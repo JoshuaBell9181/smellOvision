47dfe672a72781289eafcc0e25a574bb809880e1
Joshua Bell <josh@bellaliant.net>
2019-03-20 17:05:34 -0300

    Add play/pause button to video player

--- gui/VideoPlayer.java
+++ gui/VideoPlayer.java
@@ -5,10 +5,14 @@ import java.io.File;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;  
-import javafx.scene.Scene;  
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.media.Media;  
-import javafx.scene.media.MediaPlayer;  
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
 import javafx.scene.media.MediaView;  
 import javafx.stage.Stage;  
 
@@ -40,10 +44,30 @@ public class VideoPlayer extends Application
           
         //by setting this property to true, the Video will be played   
         mediaPlayer.setAutoPlay(true);  
+        
+         //Play and Pause button
+        Button playPause = new Button("Play/Pause");
+        playPause.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Status currentStatus = mediaPlayer.getStatus();
+				
+				if(currentStatus == Status.PLAYING) {
+					mediaPlayer.pause();
+				} else if(currentStatus == Status.PAUSED || currentStatus == Status.STOPPED) {
+					System.out.println("Player will start at:" + mediaPlayer.getCurrentTime());
+					mediaPlayer.play();
+				}
+				
+			}
+        	
+        });
           
         //setting group and scene   
         Group root = new Group();  
-        root.getChildren().add(mediaView);  
+        root.getChildren().add(mediaView);
+        root.getChildren().add(playPause);  
         Scene scene = new Scene(root,800,500);  
         primaryStage.setScene(scene);  
         primaryStage.setTitle("Playing video");  
