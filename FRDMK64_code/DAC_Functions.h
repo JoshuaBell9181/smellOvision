a5a2fa846fe3080bed47f6833b1300a3c3bb00d9
Joshua <31083115+JoshuaBell9181@users.noreply.github.com>
2019-03-30 11:07:00 -0300

    Description update

--- FRDMK64_code/DAC_Functions.h
+++ FRDMK64_code/DAC_Functions.h
@@ -1,7 +1,20 @@
 #ifndef SOURCES_DAC_FUNCTIONS_H_
 #define SOURCES_DAC_FUNCTIONS_H_
 
+/*
+ * Create By: Joshua Bell
+ * Description: Function to initialize DAC0 module
+ * Parameters: None
+ * Returns: None
+ */
 void init_DAC();
+
+/*
+ * Created By: Joshua Bell
+ * Description: Function to put a specific value on the DAC register
+ * Parameters: The integer value to be sent out the DAC
+ * Returns: None
+ */
 void convert_DAC(int dig);
 
 
