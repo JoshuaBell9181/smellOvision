d0bdc0487db383d7ceac8b385dc8d57a1cf75088
Joshua <31083115+JoshuaBell9181@users.noreply.github.com>
2019-03-30 11:16:58 -0300

    Description update

--- FRDMK64_code/Switch_Functions.h
+++ FRDMK64_code/Switch_Functions.h
@@ -1,8 +1,20 @@
 #ifndef SOURCES_Switch_FUNCTIONS_H_
 #define SOURCES_Switch_FUNCTIONS_H_
 
+/*
+ * Created By: Joshua Bell
+ * Description: Function intialize SW2 for use on the FRDMK64 board
+ * Parameters: None
+ * Returns: None
+ */
 void switch_init();
 
+/*
+ * Created By: Joshua Bell
+ * Description: Function wait until switch has been pressed
+ * Parameters: None
+ * Returns: int witch indicated a boolean value
+ */
 int switch_pressed();
 
 #endif
