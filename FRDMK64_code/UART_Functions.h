17ddff25444637c68ffd9b245be3d6076e57ad28
Joshua <31083115+JoshuaBell9181@users.noreply.github.com>
2019-03-30 11:26:04 -0300

    Description update

--- FRDMK64_code/UART_Functions.h
+++ FRDMK64_code/UART_Functions.h
@@ -1,12 +1,36 @@
 #ifndef SOURCES_UART_FUNCTIONS_H_
 #define SOURCES_UART_FUNCTIONS_H_
 
+/*
+ * Created By: Matthew MacLennan
+ * Description: Function to initialize the UART module
+ * Parameters: None
+ * Returns: None
+ */
 void init_UART();
 
+/*
+ * Created By: Joshua Bell
+ * Description: Function to put a character out to serial communication
+ * Parameters: Character to be outputted on the serial communication
+ * Returns: None
+ */
 void putChar(char character);
 
+/*
+ * Created By: Matthew MacLennan
+ * Description: Function to read a character from the serial communication
+ * Parameters: None
+ * Returns: The character that is read from the serial communication
+ */
 char getChar();
 
+/*
+ * Created By: Joshua Bell
+ * Description: Function to put a new line onto the serial communication
+ * Parameters: None
+ * Returns: None
+ */
 void putString(char * string);
 
 #endif
